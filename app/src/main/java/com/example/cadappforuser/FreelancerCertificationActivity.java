98b42be2a8aab7b169bce4a2c3099833c03b85af
Sagar <sagarjain268380@gmail.com>
2020-06-10 13:36:17 +0530

    offered

--- app/src/main/java/com/example/cadappforuser/FreelancerCertificationActivity.java
+++ app/src/main/java/com/example/cadappforuser/FreelancerCertificationActivity.java
@@ -1,14 +1,30 @@
 package com.example.cadappforuser;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FreelancerCertificationActivity extends AppCompatActivity {
 
+    Button btnCertificate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_certification);
+
+        btnCertificate=findViewById(R.id.btnCertificate);
+
+        btnCertificate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(FreelancerCertificationActivity.this,BackgoundOfFreelancerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/cadappforuser/ServicesOfferedWithprice.java
+++ app/src/main/java/com/example/cadappforuser/ServicesOfferedWithprice.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.cadappforuser.adapter.OfferedWithPriceAdapter;
 import com.example.cadappforuser.model.OfferedWithPriceModel;
@@ -13,6 +16,7 @@ import java.util.ArrayList;
 
 public class ServicesOfferedWithprice extends AppCompatActivity {
 RecyclerView recyclerView;
+Button btnOffered;
 ArrayList<OfferedWithPriceModel> offeredWithPriceModelArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,16 @@ ArrayList<OfferedWithPriceModel> offeredWithPriceModelArrayList;
         setContentView(R.layout.activity_services_offered_withprice);
         recyclerView=findViewById(R.id.recycleviewPrice);
         offeredWithPriceModelArrayList=new ArrayList<>();
+
+        btnOffered=findViewById(R.id.btnOffered);
+
+        btnOffered.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(ServicesOfferedWithprice.this,FreelancerCertificationActivity.class);
+                startActivity(intent);
+            }
+        });
 
         offeredWithPriceModelArrayList.add(new OfferedWithPriceModel("massage","fixed"));
         offeredWithPriceModelArrayList.add(new OfferedWithPriceModel("massage","fixed"));
