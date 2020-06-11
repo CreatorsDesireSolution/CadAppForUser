2073d070ee33a033bde6f3e713b0dae1012b61ff
Prachi <hr.mahak@creatorsdesire.in>
2020-06-11 11:08:52 +0530

    done
    done

--- app/src/main/java/com/example/cadappforuser/Act_Forgot_password.java
+++ app/src/main/java/com/example/cadappforuser/Act_Forgot_password.java
@@ -1,6 +1,8 @@
 package com.example.cadappforuser;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -23,6 +25,15 @@ public class Act_Forgot_password extends AppCompatActivity {
 
         chk_terms = findViewById(R.id.chk_terms);
         btn_passsubmit = findViewById(R.id.btn_passsubmit);
+
+
+        btn_passsubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SignInActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
