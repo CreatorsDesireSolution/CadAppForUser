51c470f1ba79c083686f0e6573bb77f792933c06
Sagar <sagarjain268380@gmail.com>
2020-06-12 15:36:50 +0530

    add services done

--- app/src/main/java/com/example/cadappforuser/FreelancerAddServicesActivity.java
+++ app/src/main/java/com/example/cadappforuser/FreelancerAddServicesActivity.java
@@ -1,22 +1,31 @@
 package com.example.cadappforuser;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FreelancerAddServicesActivity extends AppCompatActivity {
-
+Button btnService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.freelancer_activity_add_services);
 
-
+      btnService=findViewById(R.id.btnAddSevice);
+      btnService.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              startActivity(new Intent(FreelancerAddServicesActivity.this,FreelancherAddNewServices.class));
+          }
+      });
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         int width = displayMetrics.widthPixels;
