0f6d3ba2a5a9b90ea6b9755050b5450c3b6bc79d
Prachi <hr.mahak@creatorsdesire.in>
2020-06-19 19:25:10 +0530

    push
    push

---
+++ app/src/main/java/com/example/cadappforuser/Act_AboutUS.java
@@ -0,0 +1,14 @@
+package com.example.cadappforuser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Act_AboutUS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act__about_us);
+    }
+}
