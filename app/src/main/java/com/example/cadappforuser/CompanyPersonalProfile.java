f82de56ea2783afd5d7df686a86ca4c4205a2029
Sagar <sagarjain268380@gmail.com>
2020-06-20 17:25:11 +0530

    company profile done

---
+++ app/src/main/java/com/example/cadappforuser/CompanyPersonalProfile.java
@@ -0,0 +1,14 @@
+package com.example.cadappforuser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompanyPersonalProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company_personal_profile);
+    }
+}
