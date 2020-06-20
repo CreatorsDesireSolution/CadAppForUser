db69e05ef451e621718d873e0d5c5e8eb150bb40
Sagar <sagarjain268380@gmail.com>
2020-06-20 17:17:48 +0530

    freelancer profile

---
+++ app/src/main/java/com/example/cadappforuser/FreelancerPersonalProfile.java
@@ -0,0 +1,17 @@
+package com.example.cadappforuser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FreelancerPersonalProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_freelancer_personal_profile);
+
+
+
+    }
+}
