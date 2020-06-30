06794f3d74517796d31f6ffc2170d9177aaf633f
Prachi <hr.mahak@creatorsdesire.in>
2020-06-30 17:37:06 +0530

    done
    done

--- app/src/main/java/com/example/cadappforuser/Act_CustomerProfileEdit.java
+++ app/src/main/java/com/example/cadappforuser/Act_CustomerProfileEdit.java
@@ -2,26 +2,38 @@ package com.example.cadappforuser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.example.cadappforuser.retrofit.BaseRequest;
+
 public class Act_CustomerProfileEdit extends AppCompatActivity {
 
     ImageButton iv_edit;
+    Act_Session act_session;
+    Context context;
+    BaseRequest baseRequest;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act__customer_profile_edit);
 
+        context = this;
+
         iv_edit = findViewById(R.id.iv_edit);
+        act_session = new Act_Session(context);
 
         iv_edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+               // ApiGetRequest();
                 Intent intent = new Intent(getApplicationContext(),Act_EditCustomerInformation.class);
                 startActivity(intent);
             }
